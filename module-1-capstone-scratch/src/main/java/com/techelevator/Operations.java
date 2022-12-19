cc5c625ee8b6a2e098f56d6d0385e36c38872b58
Candice <candicekr@gmail.com>
2022-12-19 14:56:53 -0500

    finished log and change

--- module-1-capstone-scratch/src/main/java/com/techelevator/Operations.java
+++ module-1-capstone-scratch/src/main/java/com/techelevator/Operations.java
@@ -4,6 +4,11 @@ import com.techelevator.item.Inventory;
 import com.techelevator.item.Item;
 import com.techelevator.view.Menu;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -14,6 +19,7 @@ public class Operations {
     private Map<String, Item> itemMenuMap = new TreeMap<>(currentInventory.buildItemMenu());
     private double balance = 0.00;
     // private double remainingBalance;
+    private String logMessage;
 
     public Operations() {
     }
@@ -29,19 +35,25 @@ public class Operations {
     public double getBalance() {
         return balance;
     }
+
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
-    public void addToBalance (double deposit) {
+    public void addToBalance(double deposit) {
+
         double newBalance = getBalance() + deposit;
         setBalance(newBalance);
+        logMessage = String.format("%s $%.2f $%.2f", "FEED MONEY:" , deposit, newBalance);
+        log(logMessage);
+
+
     }
 
-    public void selectItemChoice () {
+    public void selectItemChoice() {
         getCurrentInventory().displayCurrentInventory();
         String userChoice = menu.itemSelectionFromUser();
-        if (isValidChoice(userChoice)){
+        if (isValidChoice(userChoice)) {
             if (validItemDetails(userChoice).getStock() > 0) {
                 itemDispenser(validItemDetails(userChoice), userChoice);
             } else {
@@ -53,20 +65,20 @@ public class Operations {
         }
     }
 
-    public boolean isValidChoice (String userChoice){
-        if (itemMenuMap.containsKey(userChoice)){
+    public boolean isValidChoice(String userChoice) {
+        if (itemMenuMap.containsKey(userChoice)) {
             validItemDetails(userChoice);
             return true;
         }
         return false;
     }
 
-    public Item validItemDetails(String userChoice){
+    public Item validItemDetails(String userChoice) {
         return this.itemMenuMap.get(userChoice);
     }
 
     // possiblely not touching itemMenuMap
-    public void itemDispenser (Item userChoice, String itemKey){
+    public void itemDispenser(Item userChoice, String itemKey) {
         String itemName = userChoice.getName();
         String itemSound = userChoice.sound();
         double itemPrice = userChoice.getPrice();
@@ -76,6 +88,8 @@ public class Operations {
             System.out.printf("\n%s %s\n%s $%.2f\n%s $%.2f\n%s\n", "Enjoy your", itemName, "Item Cost:", itemPrice, "Remaining Balance:", remainingBalance, itemSound);
             setBalance(remainingBalance);
             this.itemMenuMap.get(itemKey).reduceStock();
+            logMessage = String.format("%s %s $%.2f $%.2f", itemName , itemKey, itemPrice, getBalance());
+            log(logMessage);
             System.out.println("Thank you for your purchase. Enjoy!");
         } else {
             System.out.println("Please insert more money to purchase this item.");
@@ -85,6 +99,7 @@ public class Operations {
 
     public void change() {
         double change = getBalance() * 100;
+        double initialMoney = getBalance();
         int quarter = 0;
         int nickel = 0;
         int dime = 0;
@@ -101,9 +116,28 @@ public class Operations {
                 nickel++;
             }
         }
+        setBalance(0);
+        //add log method here when finished
+        logMessage = String.format("%s $%.2f $%.2f", "GIVE CHANGE:", initialMoney, getBalance());
+        log(logMessage);
         System.out.println("Change given:");
         System.out.printf("Number of Quarters: %3d  %n", quarter);
         System.out.printf("Number of Dimes: %6d  %n", dime);
         System.out.printf("Number of Nickels: %4d  %n", nickel);
     }
+
+
+    public void log(String message) {
+        //Displays date and time of transaction in 12 hour format with AM & PM
+        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("MM/dd/yyyy 'at' hh:mm:ss aa");
+
+        String dateTimeString = dateTimeFormat.format(new Date());
+        try (PrintWriter logger = new PrintWriter(new FileOutputStream("Log.txt", true))) {
+            logger.write(dateTimeString + " " + message + "\n");
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
 }
